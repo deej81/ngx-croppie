805520d68e7f4b72a3bbf43279f7327c7e7a577a
Josef Norlin <josef.norlin@arbetsformedlingen.se>
2019-01-17 17:07:22 +0100

    Bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, OnInit, OnChanges } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { CroppieOptions } from 'croppie';
 import { NgxCroppieComponent } from './modules/ngx-croppie/ngx-croppie.component';
 
@@ -7,27 +7,27 @@ import { NgxCroppieComponent } from './modules/ngx-croppie/ngx-croppie.component
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnChanges {
+export class AppComponent implements OnInit {
   title = 'app works';
   @ViewChild('ngxCroppie') ngxCroppie: NgxCroppieComponent;
 
   widthPx = '400';
   heightPx = '400';
-  imageUrl = '';
+  imgChangeNo = 0;
   currentImage: string;
   croppieImage: string;
+  fileName: string;
 
   public get imageToDisplay() {
     if (this.currentImage) { return this.currentImage; }
-    if (this.imageUrl) { return this.imageUrl; }
-    return `http://placehold.it/${this.widthPx}x${this.heightPx}`;
+    return `https://placehold.it/${this.widthPx}x${this.heightPx}`;
   }
 
   public get croppieOptions(): CroppieOptions {
     const opts: CroppieOptions = {};
     opts.viewport = {
       width: parseInt(this.widthPx, 10),
-      height: parseInt(this.heightPx, 10)
+      height: parseInt(this.heightPx, 10),
     };
     opts.boundary = {
       width: parseInt(this.widthPx, 10),
@@ -38,35 +38,34 @@ export class AppComponent implements OnInit, OnChanges {
   }
 
   ngOnInit() {
-    this.currentImage = this.imageUrl;
-    this.croppieImage = this.imageUrl;
   }
 
-  ngOnChanges(changes: any) {
-    if (this.croppieImage) { return; }
-    if (!changes.imageUrl) { return; }
-    if (!changes.imageUrl.previousValue && changes.imageUrl.currentValue) {
-      this.croppieImage = changes.imageUrl.currentValue;
-    }
-  }
-
-
-  // modalOpened() {
-  //   if (this.croppieImage) {
-  //     console.log('binding image to croppie');
-  //     this.ngxCroppie.bind();
-  //   }
-  // }
   newImageResultFromCroppie(img: string) {
     this.croppieImage = img;
   }
 
   saveImageFromCroppie() {
     this.currentImage = this.croppieImage;
+    this.croppieImage = null;
+  }
+
+  removeImage() {
+    this.currentImage = null;
   }
 
   cancelCroppieEdit() {
-    this.croppieImage = this.currentImage;
+    this.croppieImage = null;
+  }
+
+  rotateDir(dir: string) { // dir: direction = {'l', 'r'}
+    if (!this.croppieImage) { return; }
+    const rotateLeft = 90;
+    const rotateRight = -90;
+    if (dir === 'l') {
+      this.ngxCroppie.rotate(rotateLeft);
+      return;
+    }
+    this.ngxCroppie.rotate(rotateRight);
   }
 
   imageUploadEvent(evt: any) {
@@ -74,10 +73,12 @@ export class AppComponent implements OnInit, OnChanges {
     if (!evt.target.files) { return; }
     if (evt.target.files.length !== 1) { return; }
     const file = evt.target.files[0];
-    if (file.type !== 'image/jpeg' && file.type !== 'image/png' && file.type !== 'image/gif' && file.type !== 'image/jpg') { return; }
+    this.fileName = file.name;
+    if (file.type !== 'image/jpeg' && file.type !== 'image/png' && file.type !== 'image/jpg') { return; }
     const fr = new FileReader();
-    fr.onloadend = (loadEvent) => {
-      this.croppieImage = fr.result;
+    fr.onloadend = (loadEvent: any) => {
+      this.croppieImage = loadEvent.target.result;
+      this.imgChangeNo++;
     };
     fr.readAsDataURL(file);
   }
--- src/app/modules/ngx-croppie/ngx-croppie.component.ts
+++ src/app/modules/ngx-croppie/ngx-croppie.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 import * as Croppie from 'croppie';
 import { CroppieOptions, ResultOptions, CropData } from 'croppie';
@@ -6,44 +6,53 @@ import { CroppieOptions, ResultOptions, CropData } from 'croppie';
 export type Type = 'canvas' | 'base64' | 'html' | 'blob' | 'rawcanvas';
 
 @Component({
-    // tslint:disable-next-line:component-selector
-    selector: 'ngx-croppie',
-    template: `<div #imageEdit (update)="newResult()"></div>`
+  // tslint:disable-next-line:component-selector
+  selector: 'ngx-croppie',
+  template: `<div #imageEdit (update)="newResult()"></div>`
 })
-export class NgxCroppieComponent implements OnInit {
-    @ViewChild('imageEdit') imageEdit: ElementRef;
-    @Input() croppieOptions: CroppieOptions;
-    @Input() imageUrl: string;
-    @Input() points: number[];
-    @Input() bind: (img: string) => void;
-    @Input() outputFormatOptions: ResultOptions = { type: 'base64', size: 'viewport' };
-    @Output() result: EventEmitter<string | HTMLElement | Blob | HTMLCanvasElement>
-                    = new EventEmitter<string | HTMLElement | Blob | HTMLCanvasElement>();
-    private _croppie: Croppie;
-    ngOnInit(): void {
-        // https://github.com/Foliotek/Croppie/issues/470 :-( )
-        this._croppie = new Croppie['Croppie'](this.imageEdit.nativeElement, this.croppieOptions);
+export class NgxCroppieComponent implements OnInit, OnChanges {
+  @ViewChild('imageEdit') imageEdit: ElementRef;
+  @Input() croppieOptions: CroppieOptions;
+  @Input() imageUrl: string;
+  @Input() points: number[];
+  @Input() imgChangeNo: number;
 
+  @Input() outputFormatOptions: ResultOptions = { type: 'base64', size: 'viewport' };
+  @Output() result: EventEmitter<string | HTMLElement | Blob | HTMLCanvasElement>
+    = new EventEmitter<string | HTMLElement | Blob | HTMLCanvasElement>();
+  private _croppie: Croppie;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    // https://github.com/Foliotek/Croppie/issues/470 :-( )
+    this._croppie = new Croppie['Croppie'](this.imageEdit.nativeElement, this.croppieOptions);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.imgChangeNo) {
+      setTimeout(() => {
         this._croppie.bind({
-            url: this.imageUrl, points: this.points
+          url: changes.imageUrl.currentValue,
+          orientation: 1
         });
-        this.bind = (img: string) => {
-            this._croppie.bind({ url: this.imageUrl, points: this.points });
-        };
+      });
     }
+  }
 
-    newResult() {
-        this._croppie.result(this.outputFormatOptions).then((res) => {
-            this.result.emit(res);
-        });
-    }
+  newResult() {
+    this._croppie.result(this.outputFormatOptions).then((res) => {
+      this.result.emit(res);
+    });
+  }
 
-    rotate(degrees: 90 | 180 | 270 | -90 | -180 | -270) {
-        this._croppie.rotate(degrees);
-    }
+  rotate(degrees: 90 | 180 | 270 | -90 | -180 | -270) {
+    this._croppie.rotate(degrees);
+  }
 
   get(): CropData {
-      return this._croppie.get();
+    return this._croppie.get();
   }
 
 }
